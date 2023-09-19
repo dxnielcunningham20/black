aa6989f7d3f08f7e8ad6512f6170e0f01b1f5082
dxnielcunningham <carlosgarcia67jusj@gmail.com>
2023-09-19 15:13:48 -0600

    inicializa en este repositorio

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+app.get('/', (req, res) => {
+  res.send('¡Hola, mundo!');
+});
+app.listen(port, () => {
+  console.log(`El servidor está escuchando en http://localhost:${8000}`);
+});
